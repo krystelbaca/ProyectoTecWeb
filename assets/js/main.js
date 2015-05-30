c64d38a1a15c3a8749fb22135099086e66fd2441
EdgarOPG <eopg95@gmail.com>
2015-05-30 16:29:08 -0600

    Actualizacion 8
    Adicion de librerias para animacion.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,9 @@ $(document).ready(function()
 
 		$('.navItem').animo('blur', {duration: .2, amount: .9});
 
-
-		$('#cuadro').click(function()
-		{
-			$(this).animo( { animation: 'tada' } );
+		setInterval(function() {
+      	$('.icon').animo( { animation: 'tada' } );
 			console.log("animacion aplicada a icon");
-		});
+		}, 2000);
+
 });
