5abb30bf0fcfb97e5a8b80842d5b28a009fe4a5b
EdgarOPG <eopg95@gmail.com>
2015-05-30 17:23:07 -0600

    Actualizacion 9
    Pulimiento de animaciones en networks.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,20 @@ $(document).ready(function()
 
 		$('.navItem').animo('blur', {duration: .2, amount: .9});
 
-		setInterval(function() {
-      	$('.icon').animo( { animation: 'tada' } );
+		setInterval(function interval() 
+		{
+      		$('.animacion').animo( { animation: 'tada' } );
 			console.log("animacion aplicada a icon");
 		}, 2000);
 
+		$('.icon').mouseenter(function()
+		{
+			$(this).animo( { animation: 'bounce' } );
+			$(this).removeClass('animacion');
+    	});
+
+    	$('.icon').mouseleave(function()
+		{
+			$(this).addClass('animacion');
+    	});
 });
