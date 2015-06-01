3bba1f082b9663e8d3c66b0df31ead53661d617a
Krystel <salayandia.k@gmail.com>
2015-06-01 05:19:43 -0600

    galeria

--- assets/js/galerias.js
+++ assets/js/galerias.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
-var urlPortada = "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=ce4c4ab1f01efa66fe9fa6bc41aadf31&user_id=133697780%40N04&format=json&nojsoncallback=1";
-		$.getJSON(urlPortada, function(data){
-			 $.each(data.photos.photo, function(i,item){
-		        src = "http://farm"+ item.farm +".static.flickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +"_m.jpg";
-			 	$("<img/>").attr({"src":src, "id":"portada"}).appendTo("#wrapper");
-		        if ( i == 3 ) return false;
+var urlGaleria = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=656853a74877e957ff7ff234bbd7b2ce&photoset_id=72157653406095919&user_id=133697780%40N04&format=json&nojsoncallback=1";
+		$.getJSON(urlGaleria, function(data){
 
+			 $.each(data.photoset.photo, function(i,item){
+		        src = "http://farm"+ item.farm +".static.flickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +"_b.jpg";
+			 	$("<img/>").attr({"src":src, "id":"galeria"}).appendTo("#wrapper");
+		       
 
 		        
 
